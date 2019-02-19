1b23583371856ddf676799fc563509dd3fca58d5
mohanraj-iteron <mohanraj.k@iteron.in>
2019-02-19 09:15:23 +0530

    rails api application

---
+++ README.md
@@ -0,0 +1,18 @@
+``` Ruby Version
+
+ruby-2.5.1
+
+```
+
+
+```` Server Start
+
+rails s -p 3000
+
+````
+
+```` Create DB
+
+rake db:create db:migrate db:seed
+
+````
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "mohan", email: "mohan@gmail.com", username: "mohan", password: "passwrod", password_confirmation: "password")
+User.create(name: "raj", email: "raj@gmail.com",username: "raj", password: "raj@gmail.com", password_confirmation: "raj@gmail.com")
+User.create(name: "samy", email: "samy@gmail.com", username: "samy", password: "samy@gmail.com", password_confirmation: "samy@gmail.com")
+User.create(name: "vijay", email: "vijay@gmail.com", username: "vijay", password: "vijay@gmail.com", password_confirmation: "vijay@gmail.com")
